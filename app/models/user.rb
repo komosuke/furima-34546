ca6713edda54e68bf2212e76c99b0881c8f5b1a8
komosuke <uw.love0503@gmail.com>
2021-02-18 14:27:53 +0900

    新規登録、ログイン、ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+
 end
